f3959ef2fe286a95442a19506a55ea74bd0564a3
ThunderGer <Jaames11@hotmail.com>
2022-11-11 01:23:45 -0600

    Agregando imagen a la DB prueba 3

--- config/db.py
+++ config/db.py
@@ -1,5 +1,6 @@
 # Connecting to the MongoDB database.
 from pymongo import MongoClient
 #from config.keys import MongoCli
+from os import environ
 
-conn = MongoClient(MONGO_CLI)
+conn = MongoClient(environ['MONGO_CLI'])
