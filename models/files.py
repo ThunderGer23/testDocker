15541d740f37fe2ecb9dc023c88fc60798fb8023
ThunderGer <Jaames11@hotmail.com>
2022-11-11 01:20:03 -0600

    Agregando imagen a la DB prueba 2

--- models/files.py
+++ models/files.py
@@ -1,7 +1,7 @@
-from typing import Optional, Binary
+from typing import Optional
 from pydantic import BaseModel
 
 class Files(BaseModel):
     id: Optional[str]
     name: str
-    data: Binary
+    data: bytes
